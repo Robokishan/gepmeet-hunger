de19f70756a54c0c1f24961c7adc7040ceb40046
Kishan Joshi <robokishan.blogspot@gmail.com>
2022-01-09 21:35:33 +0530

    dragable to tsx

--- gepmeet/pages/draggables/Draggable.tsx
+++ gepmeet/pages/draggables/Draggable.tsx
@@ -1,37 +1,32 @@
-import { Rnd, RndDragCallback, RndResizeCallback } from "react-rnd";
-import React from "react";
-import { v4 } from 'uuid'
-import _ from 'lodash'
-class Draggable extends React.Component {
-    constructor(props){
-        super(props);
-    }
+import { ReactElement } from "react";
+import { Rnd } from "react-rnd";
+import _ from "lodash";
 
-    render() {
-        
-        let _drag_disable = !this.props.isLocal;
-        
-        return (
-            <Rnd
-                onDrag={this.props.handleDrag}
-                // onDragStop={() => {dispatch(dragResizeSlice.actions.setDragging(false))}}
-                disableDragging={_drag_disable}
-                enableResizing={false}
-                // scale={zoom}
-                // position={{x: position.x, y: position.y}}
-                size={{width: 128, height: 128}}
-                className="box interactive"
-                bounds='#canvas'
-                style={{
-                borderRadius: '50%'
-                }}
-            >
-                <div className="content">
-                    {this.props.children}
-                </div>
-            </Rnd>
-        );
-    }
+interface Props {
+  isLocal: boolean;
+  handleDrag: any;
 }
 
-export default Draggable;
+export default function Draggable({
+  isLocal,
+  handleDrag,
+}: Props): ReactElement {
+  return (
+    <Rnd
+      onDrag={handleDrag}
+      // onDragStop={() => {dispatch(dragResizeSlice.actions.setDragging(false))}}
+      disableDragging={isLocal}
+      enableResizing={false}
+      // scale={zoom}
+      // position={{x: position.x, y: position.y}}
+      size={{ width: 128, height: 128 }}
+      className="box interactive"
+      bounds="#canvas"
+      style={{
+        borderRadius: "50%",
+      }}
+    >
+      <div className="content">{this.props.children}</div>
+    </Rnd>
+  );
+}
