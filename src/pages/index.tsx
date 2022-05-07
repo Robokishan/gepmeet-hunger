879a7650159e3fd9b36f8b3d9064977dff81b84c
Kishan Joshi <robokishan.blogspot@gmail.com>
2022-05-08 00:10:22 +0530

    add: video player

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,13 +1,18 @@
-import { Link as ChakraLink, Text, Flex, Button, Box } from "@chakra-ui/react";
+import { Input, Text, Flex, Button } from "@chakra-ui/react";
 import { Device } from "mediasoup-client";
 import { Container } from "../components/Container";
 import { DarkModeSwitch } from "../components/DarkModeSwitch";
 import useAxiosClient from "../utils/useAxiosClient";
 import { API } from "../utils/api";
 import { RTPType } from "../utils/rtpTypes";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
+import { ArrowRightIcon } from "@chakra-ui/icons";
+import axiosClient from "../utils/axiosClient";
 
 const Index = () => {
+  const [roomId, setroomId] = useState("");
+  const [isError, setIsError] = useState(false);
+
   let device;
 
   const { data, isLoading } = useAxiosClient<RTPType>({
@@ -34,6 +39,26 @@ const Index = () => {
     await device.load({ routerRtpCapabilities });
   }
 
+  const connectRoom = async () => {
+    if (!roomId) {
+      console.log("Room not found");
+      setIsError(true);
+    } else {
+      try {
+        const { data } = await axiosClient.post(API.CREATE_ROOM);
+        console.log(data);
+        setIsError(false);
+      } catch (error) {
+        setIsError(true);
+        console.error(error);
+      }
+    }
+  };
+
+  const createRoom = async () => {
+    console.log("Creating Room");
+  };
+
   return (
     <Container height="100vh">
       <Flex margin="50px 0px 0px 0px" direction="row" height="100%">
@@ -41,6 +66,30 @@ const Index = () => {
         {!isLoading && data && (
           <Flex alignItems="center" gap="50px" direction="column">
             <Text>Worker ID: {data.worker}</Text>
+            <Flex gap="10px">
+              <Input
+                variant="outline"
+                isInvalid={isError}
+                placeholder="Enter Room ID"
+                onChange={(e) => setroomId(e.target.value)}
+              />
+
+              <Button
+                colorScheme={isError ? "red" : "facebook"}
+                onClick={connectRoom}
+              >
+                <ArrowRightIcon />
+              </Button>
+            </Flex>
+            <Flex>
+              <Button
+                colorScheme="messenger"
+                rounded="button"
+                onClick={createRoom}
+              >
+                Create Room
+              </Button>
+            </Flex>
             <Flex
               borderRadius="20px"
               padding="10px"
@@ -57,7 +106,7 @@ const Index = () => {
               <video id="remote_video" controls></video>
             </Flex>
 
-            <Flex gap="10px" alignItems="center">
+            <Flex margin="10px 0px 100px 0px" gap="10px" alignItems="center">
               <Button variant="solid" colorScheme="green" rounded="button">
                 Connect
               </Button>
--- src/utils/api.ts
+++ src/utils/api.ts
@@ -1,3 +1,4 @@
 export enum API {
   GET_RTP_CAPABILITIES = "/api/v1/router/getrtp",
+  CREATE_ROOM = "/api/v1/create",
 }
