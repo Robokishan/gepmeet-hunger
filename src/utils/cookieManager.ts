8bd60b2a69ee47d40eee9771f031f8badf8d8889
Kishan Joshi <robokishan.blogspot@gmail.com>
2022-06-22 15:14:37 +0530

    Update cookieManager.ts

--- src/utils/cookieManager.ts
+++ src/utils/cookieManager.ts
@@ -18,14 +18,16 @@ export function setCookie(name: string, val: string, expires = 1): void {
 
 export function getCookie(name: string): string {
   if (typeof window !== "undefined") {
-    const value = "; " + document.cookie;
-    const parts = value.split("; " + name + "=");
-    if (parts.length == 2) {
-      return parts.pop().split(";").shift();
-    }
+    // const value = "; " + document.cookie;
+    // const parts = value.split("; " + name + "=");
+    // if (parts.length == 2) {
+    //   return parts.pop().split(";").shift();
+    // }
+    return localStorage.getItem("token");
   }
 }
 
 export function deleteCookie(name: string): void {
-  document.cookie = name + "=; expires=Thu, 01 Jan 1970 00:00:01 GMT; path=/";
+  // document.cookie = name + "=; expires=Thu, 01 Jan 1970 00:00:01 GMT; path=/";
+  localStorage.clear();
 }
